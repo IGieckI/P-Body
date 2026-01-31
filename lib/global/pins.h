ae14b8b5c4ea0876b6d8ed5364cfe0a0f1e49800
IGieckI <giacomo.antonelli3@gmail.com>
2026-01-31 17:37:34 +0100

    Defined global file for  configuration variables

---
+++ lib/global/pins.h
@@ -0,0 +1,14 @@
+#pragma once
+
+// Spin-direction pins for each robot wheel
+#define WHEEL_TOP_LEFT_PIN_1 3
+#define WHEEL_TOP_LEFT_PIN_2 4
+
+#define WHEEL_TOP_RIGHT_PIN_1 5
+#define WHEEL_TOP_RIGHT_PIN_2 6
+
+#define WHEEL_BOTTOM_LEFT_PIN_1 14
+#define WHEEL_BOTTOM_LEFT_PIN_2 15
+
+#define WHEEL_BOTTOM_RIGHT_PIN_1 16
+#define WHEEL_BOTTOM_RIGHT_PIN_2 17
